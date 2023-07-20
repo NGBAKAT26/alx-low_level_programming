49f6d92924a34d4bc50edc58ca3fca68c1654590
NGBAKAT26 <katlehopheko28@gmail.com>
2023-07-20 09:57:54 -0700

    Multiple mains

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+void positive_or_negative(int i)
+{
+	if (i > 0)
+		print("*%d is positive\n", i);
+	
+	else if (i < 0)
+		printf("%d is negative\n", j);
+
+	else
+		printf("%d is zero\n", i);
+}
